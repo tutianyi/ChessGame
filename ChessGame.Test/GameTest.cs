d68542f929666e4d2881e7209e2fb84b448d6880
blood20 <blood20@sina.com>
2017-05-23 23:29:01 +0800

    .

---
+++ ChessGame.Test/GameTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ChessGame.Test
+{
+    [TestFixture]
+    public class GameTest
+    {
+        [Test]
+        public void test_default_move()
+        {
+            Game game = new Game("XOXOX-OXO");
+            Assert.AreEqual(5, game.Move('X'));
+
+            game = new Game("XOXOXOOX-");
+            Assert.AreEqual(8, game.Move('O'));
+
+            game = new Game("---------");
+            Assert.AreEqual(0, game.Move('X'));
+
+            game = new Game("XXXXXXXXX");
+            Assert.AreEqual(-1, game.Move('X'));
+        }
+
+        [Test]
+        public void test_find_winning_move()
+        {
+            Game game = new Game("XO-XX-OOX");
+            Assert.AreEqual(5, game.Move('X'));
+        }
+
+        [Test]
+        public void test_win_conditions()
+        {
+            Game game = new Game("---XXX---");
+            Assert.AreEqual('X', game.winner());
+        }
+    }
+}
---
+++ ChessGame/Game.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+
+namespace ChessGame
+{
+    public class Game
+    {
+        private StringBuilder _board;
+
+        public Game(string board)
+        {
+            _board = new StringBuilder(board);
+        }
+
+        public Game(StringBuilder board, int position, char player)
+        {
+            _board = new StringBuilder();
+            _board.Append(board);
+            _board[position] = player;
+        }
+
+        public int Move(char player)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (_board[i] == '-')
+                {
+                    Game game = play(i, player);
+                    if (game.winner() == player)
+                        return i;
+                }
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (_board[i] == '-')
+                    return i;
+            }
+            return -1;
+        }
+
+        public Game play(int i, char player)
+        {
+            return new Game(this._board, i, player);
+        }
+
+        public char winner()
+        {
+            if (_board[0] != '-'
+                && _board[0] == _board[1]
+                && _board[1] == _board[2])
+                return _board[0];
+            if (_board[3] != '-'
+                && _board[3] == _board[4]
+                && _board[4] == _board[5])
+                return _board[3];
+            if (_board[6] != '-'
+                && _board[6] == _board[7]
+                && _board[7] == _board[8])
+                return _board[6];
+            return '-';
+        }
+    }
+}
