94e0154ae3d7fed05edacf664de12cb4440620f0
tutianyi <blood20@sina.com>
2017-06-09 09:36:20 +0800

    代码重构

--- ChessGame/Game.cs
+++ ChessGame/Game.cs
@@ -4,6 +4,7 @@ namespace ChessGame
 {
     public class Game
     {
+        public const int None = -1;
         private StringBuilder _board;
 
         public Game(string board)
@@ -20,44 +21,65 @@ namespace ChessGame
 
         public int Move(char player)
         {
+            var position = None;
+            if (CanWin(player, ref position))
+                return position;
+            if (CanMove(ref position))
+                return position;
+            return position;
+        }
+
+        private bool CanWin(char player, ref int position)
+        {
             for (int i = 0; i < 9; i++)
             {
                 if (_board[i] == '-')
                 {
-                    Game game = play(i, player);
-                    if (game.winner() == player)
-                        return i;
+                    Game game = Play(i, player);
+                    if (game.Winner() == player)
+                    {
+                        position = i;
+                        return true;
+                    }
                 }
             }
+            return false;
+        }
 
+        private bool CanMove(ref int position)
+        {
             for (int i = 0; i < 9; i++)
             {
                 if (_board[i] == '-')
-                    return i;
+                {
+                    position = i;
+                    return true;
+                }
             }
-            return -1;
+            return false;
         }
 
-        public Game play(int i, char player)
+        public Game Play(int i, char player)
         {
             return new Game(this._board, i, player);
         }
 
-        public char winner()
+        public char Winner()
         {
-            if (_board[0] != '-'
-                && _board[0] == _board[1]
-                && _board[1] == _board[2])
+            if (IsLine(0))
                 return _board[0];
-            if (_board[3] != '-'
-                && _board[3] == _board[4]
-                && _board[4] == _board[5])
+            if (IsLine(3))
                 return _board[3];
-            if (_board[6] != '-'
-                && _board[6] == _board[7]
-                && _board[7] == _board[8])
+            if (IsLine(6))
                 return _board[6];
             return '-';
+        }
+
+        private bool IsLine(int index)
+        {
+            return _board[index] != '-'
+                   && _board[index] == _board[index + 1]
+                   && _board[index + 1] == _board[index + 2];
         }
     }
 }
